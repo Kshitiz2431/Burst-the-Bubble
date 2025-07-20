c0fefa1b943c3a0eb360fc6a9acd273917fbeae4
Kshitiz2431 <kshitizagarwal2402@gmail.com>
2025-07-21 04:43:40 +0530

    Added newsletter confirmation page

---
+++ app/newsletter/success/page.tsx
@@ -0,0 +1,14 @@
+
+
+export default function VerifiedPage() {
+  return (
+    <div>
+      <main className="p-20 ml-100 mt-20 ml-25">
+        <h2 className="text-2xl font-semibold mb-4">Email Verified</h2>
+        <p className="text-lg text-gray-700">
+          Thank you! Your Email has been successfully verified.
+        </p>
+      </main>
+    </div>
+  );
+}
